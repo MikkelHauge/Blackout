fce0047518e84ad4ebb761850df6c0e35fff8164
Mikkel <Mikkel@8700K>
2023-03-09 15:17:34 +0100

    Add project files.

---
+++ BlackoutWin/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace BlackoutWin
+{
+    partial class Blackout
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Blackout));
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            detectFullscreenTimer = new System.Windows.Forms.Timer(components);
+            labelinfo = new Label();
+            notifyIcon1 = new NotifyIcon(components);
+            contextMenuStripSystemTray = new ContextMenuStrip(components);
+            restoreWindowToolStripMenuItem = new ToolStripMenuItem();
+            closeApplicationToolStripMenuItem = new ToolStripMenuItem();
+            contextMenuStripSystemTray.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaptionText;
+            button1.Font = new Font("Calisto MT", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.AppWorkspace;
+            button1.Location = new Point(12, 325);
+            button1.Name = "button1";
+            button1.Size = new Size(158, 74);
+            button1.TabIndex = 0;
+            button1.Text = "Activate";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            button1.MouseEnter += highlighter;
+            button1.MouseLeave += highlighter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.AppWorkspace;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(147, 90);
+            label1.TabIndex = 1;
+            label1.Text = "Click the 'Activate' button \r\nto start detecting \r\nfullscreen applications. \r\n\r\nClick Deactivate \r\nto... deactivate. ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.AppWorkspace;
+            label2.Location = new Point(12, 130);
+            label2.Name = "label2";
+            label2.Size = new Size(117, 45);
+            label2.TabIndex = 2;
+            label2.Text = "The program runs in \r\nsystem tray when \r\nyou minimize it.";
+            // 
+            // detectFullscreenTimer
+            // 
+            detectFullscreenTimer.Interval = 1000;
+            detectFullscreenTimer.Tick += detectFullscreenTimer_Tick;
+            // 
+            // labelinfo
+            // 
+            labelinfo.AutoSize = true;
+            labelinfo.ForeColor = SystemColors.AppWorkspace;
+            labelinfo.Location = new Point(12, 205);
+            labelinfo.Name = "labelinfo";
+            labelinfo.Size = new Size(0, 15);
+            labelinfo.TabIndex = 3;
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipText = "TIP TEXT";
+            notifyIcon1.BalloonTipTitle = "Title! :D";
+            notifyIcon1.ContextMenuStrip = contextMenuStripSystemTray;
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            notifyIcon1.MouseDoubleClick += notifyIcon1_MouseDoubleClick;
+            // 
+            // contextMenuStripSystemTray
+            // 
+            contextMenuStripSystemTray.Items.AddRange(new ToolStripItem[] { restoreWindowToolStripMenuItem, closeApplicationToolStripMenuItem });
+            contextMenuStripSystemTray.Name = "contextMenuStripSystemTray";
+            contextMenuStripSystemTray.Size = new Size(168, 48);
+            // 
+            // restoreWindowToolStripMenuItem
+            // 
+            restoreWindowToolStripMenuItem.Name = "restoreWindowToolStripMenuItem";
+            restoreWindowToolStripMenuItem.Size = new Size(167, 22);
+            restoreWindowToolStripMenuItem.Text = "Restore Window";
+            restoreWindowToolStripMenuItem.Click += restoreWindowMenuItem_click;
+            // 
+            // closeApplicationToolStripMenuItem
+            // 
+            closeApplicationToolStripMenuItem.Name = "closeApplicationToolStripMenuItem";
+            closeApplicationToolStripMenuItem.Size = new Size(167, 22);
+            closeApplicationToolStripMenuItem.Text = "Close Application";
+            closeApplicationToolStripMenuItem.Click += closeApplicationSystemTray_Click;
+            // 
+            // Blackout
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(187, 412);
+            Controls.Add(labelinfo);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Blackout";
+            Text = "Blackout";
+            FormClosing += MainForm_FormClosing;
+            contextMenuStripSystemTray.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1; // den eneste knap!
+        private Label label1; // øverste label
+        private Label label2; // nederste label
+        private System.Windows.Forms.Timer detectFullscreenTimer;
+        private Label labelinfo;
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStripSystemTray;
+        private ToolStripMenuItem restoreWindowToolStripMenuItem;
+        private ToolStripMenuItem closeApplicationToolStripMenuItem;
+    }
+}
---
+++ BlackoutWin/Form1.cs
@@ -0,0 +1,246 @@
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System;
+using static System.TimeZoneInfo;
+
+
+namespace BlackoutWin
+{
+    public partial class Blackout : Form
+    {
+        public static class Win32API
+        {
+            public const int SPI_SETDESKWALLPAPER = 20;
+            public const int SPIF_UPDATEINIFILE = 0x01;
+            public const int SPIF_SENDWININICHANGE = 0x02;
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern IntPtr GetForegroundWindow();
+
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool GetWindowRect(IntPtr hwnd, out Rect lpRect);
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct Rect
+            {
+                public int Left;
+                public int Top;
+                public int Right;
+                public int Bottom;
+
+                public int Width { get { return Right - Left; } }
+                public int Height { get { return Bottom - Top; } }
+            }
+
+
+        }
+        private bool isHighLighted = false;
+        private const int SPI_SETDESKWALLPAPER = 20;
+        private const int SPIF_UPDATEINIFILE = 0x01;
+        private const int SPI_SETSLIDESHOW = 0x1018;
+        private const int SS_USERSELECTED = 0x0004;
+        private const int SPIF_SENDWININICHANGE = 0x02;
+        private bool activated = false;
+        private bool hasBeenFullscreen = false;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern bool SystemParametersInfo(uint uiAction, uint uiParam, IntPtr pvParam, uint fWinIni);
+
+
+        public Blackout()
+        {
+            InitializeComponent();
+            notifyIcon1.Text = "Blackout";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { //"activate" knappen. den eneste knap!
+
+            if (!activated)
+            {
+                activated = true;
+                button1.Text = "Deactivate";
+                detectFullscreenTimer.Enabled = true;
+                detectFullscreenTimer.Start();
+                notifyIcon1.Text = "Fullscreen detection started...";
+
+            }
+            else
+            {
+                activated = false;
+                button1.Text = "Activate";
+                detectFullscreenTimer.Enabled = false;
+                detectFullscreenTimer.Stop();
+                notifyIcon1.Text = "Fullscreen detection deactivated...";
+            }
+        }
+
+        private void detectFullscreenTimer_Tick(object sender, EventArgs e)
+        {
+            bool fullscreenDetected = false;
+            for (int i = 0; i < Screen.AllScreens.Length; i++)
+            {
+                Rectangle bounds = Screen.AllScreens[i].Bounds;
+                IntPtr handle = Win32API.GetForegroundWindow();
+                if (Win32API.GetWindowRect(handle, out Win32API.Rect rect) && rect.Width == bounds.Width && rect.Height == bounds.Height)
+                {
+                    labelinfo.Text = "Fullscreen detection:\nFullscreen!\nSetting Wallpapers to black!";
+                    fullscreenDetected = true;
+                    SetWallpaperToBlack();
+                    hasBeenFullscreen = true;
+                }
+            }
+            if (!fullscreenDetected )
+            {
+                if(!hasBeenFullscreen){
+                    labelinfo.Text = "Fullscreen detection:\nNot detected!";
+
+                } else
+                {
+                    labelinfo.Text = "Fullscreen detection:\nNone!\nSetting Wallpapers to slideshow!";
+                    hasBeenFullscreen = false;
+                    SetWallpaperBackToSlideshow();
+                }
+            }
+        }
+
+        private void SetWallpaperToBlack()
+        {
+            string imagePath = AppDomain.CurrentDomain.BaseDirectory + "black.jpg";
+            IntPtr imagePathPtr = Marshal.StringToHGlobalUni(imagePath);
+            try
+            {
+                SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, imagePathPtr, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(imagePathPtr);
+            }
+        }
+
+        private void SetWallpaperBackToSlideshow()
+        {
+            string folderPath = @"F:\Pictures\Wallpapers";
+            string[] wallpapers = Directory.GetFiles(folderPath);
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\PerMonitorSettings", true);
+            if (wallpapers.Length > 0)
+            {
+                // Set a random wallpaper for each monitor
+  
+                    int randomIndex = new Random().Next(0, wallpapers.Length);
+                    string randomWallpaper = wallpapers[randomIndex];
+
+                    // Set the wallpaper for the current screen
+                    SetWallpaper(randomWallpaper);
+            }
+            else
+            {
+                // Handle the case where there are no wallpaper files
+                labelinfo.Text = "Ingen wallpapers fundet i mappen:\n" + @"F:\Pictures\Wallpapers";
+            }
+            
+
+        }
+
+        private void SetWallpaper(string wallpaperPath)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            // Set wallpaper style to "Fit"
+            key.SetValue(@"WallpaperStyle", "10");
+
+            // Set wallpaper tiling to "Tile"
+            key.SetValue(@"TileWallpaper", "0");
+            // Set the new wallpaper
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, wallpaperPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            notifyIcon1.Visible = true;
+            this.WindowState = FormWindowState.Minimized; // Add this line
+            this.ShowInTaskbar = false;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipTitle = "Blackout";
+            notifyIcon1.BalloonTipText = "Blackout has been minimized to the system tray.\nYou can only close it from here.";
+
+            shutDownProcedure(sender, e);
+        }
+
+        private void shutDownProcedure(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (FormWindowState.Minimized == this.WindowState)
+                {
+                    notifyIcon1.Visible = true;
+                    if (!IsDisposed)
+                    {
+                        notifyIcon1.ShowBalloonTip(500);
+                    }
+                }
+                else if (FormWindowState.Normal == this.WindowState)
+                {
+                    notifyIcon1.Visible = false;
+                    this.ShowInTaskbar = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show(); // Restore the form
+            this.WindowState = FormWindowState.Normal; // Restore the form's window state
+            notifyIcon1.Visible = false; // Hide the system tray icon
+        }
+
+        private void restoreWindowMenuItem_click(object sender, EventArgs e)
+        {
+            this.Show(); // Restore the form
+            this.WindowState = FormWindowState.Normal; // Restore the form's window state
+            notifyIcon1.Visible = false; // Hide the system tray icon
+        }
+
+        private void closeApplicationSystemTray_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Dispose();
+            activated = false; // deaktivér
+            button1_Click(sender, e); // burde sætte wallpaper osv, til slideshow igen, fordi "activated" er false
+            SetWallpaperBackToSlideshow(); // sætter slideshow i gang igen, hvis brugeren ikke selv gjorde det.
+            Application.Exit(); // lukker programmet.
+        }
+
+
+        // highlighting af text, til activate-knappen
+        private void highlighter(object sender, EventArgs e)
+        {
+            if (!isHighLighted)
+            {
+                button1.ForeColor = Color.White;
+                isHighLighted = true;
+            }
+            else
+            {
+                button1.ForeColor = Color.DarkGray;
+                isHighLighted = false;
+            }
+        }
+    }
+}
